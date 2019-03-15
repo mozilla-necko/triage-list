48118e91f6b0f3544ff5e7311b03c49f94b77da5
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-03-15 15:05:00 +0100

    move to npm based local update and publich system

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # This is the human/machine readable list of Mozilla Necko team triagers and dates each one is on duty.
 
-The core is a hand-maintained JSON file [triage.json](https://mozilla-necko.github.io/triage-list/triage.json) with list of names and bugzilla emails, and a calendar.
-
+## To read the triage list and calendar
+ refer to regularly updated JSON file [triage.json](https://mozilla-necko.github.io/triage-list/triage.json) with list of names and bugzilla emails, and a calendar with duty end dates.
 - `"triagers"` root object node holds a map of `"Full Name" -> { data }`.  Right now the `data` object keeps only the bugzilla email in `bzmail` field.
-- `"duty-end-dates"` root holds a map of `date -> "Full Name"`, where the `date` is the last day the triager is on duty, inclusive.
+- `"duty-end-dates"` root holds a map of `date -> "Full Name"`, where the `date` is the last day the triager is on duty, inclusive.  The last date is `2035-01-01` pointing at the `Fallback` triager.  Getting there means the list has to be manually updated and re-published.
 
-If the consumer can use JavaScript, there is [triage.js](https://mozilla-necko.github.io/triage-list/triage.js) script file with few helper functions to async-fetch the JSON (`fetchTriage()`) and to get the current triager (`currentTriager(json)`).  Result of `currentTriager` is an object with properties `name`, `till` being a date string YYYY-MM-DD with the ending date, and `bzmail` with the bugzilla email to nag.
+## To see triagers in your google calendar
+Just use [this ICS link](https://calendar.google.com/calendar/r?cid=http://mozilla-necko.github.io/triage-list/necko-triage.ics) and follow instructions.
 
-And to quickly view the current triage duty, go to [triager.html](https://mozilla-necko.github.io/triage-list/triager.html)
+## To update the triage list
+```
+$git clone https://github.com/mozilla-necko/triage-list.git
+$cd triage-list
+$npm install
+$npm run update
+$npm run push
+```
+
+`npm run update` will automatically append the next full cycle of all triagers to the `triage.json` file.  `npm run push` will update the ICS file and `git push` the changes.  Then you are done.
+
+If you already cloned before, then `update` and `push` commands are enough to publicly update the list.
--- triage.js
+++ triage.js
@@ -1,22 +1,161 @@
-async function fetchTriage() {
-  let request = await fetch("triage.json");
-  let json = await request.json();
-  return json;
+const fs = require('fs');
+
+const help_string =
+  `Usage: npm run <command> -- [arguments]
+commands
+  * update: automatically extends the json calendar with triagers using the default list, adds only one cycle
+  * prepush: updates the ICS generated file with data from the json calendar (called before publish automatically)
+  * exempt -- <date>: removes a triager on duty at the specified <date> from the calendar and shifts all triagers
+  * push: pushes the changes to the repo and makes them public, refreshes the ics file as well`;
+
+const TRIAGE_JSON_FILE = "triage.json";
+const TRIAGE_ICAL_FILE = "necko-triage.ics";
+const FALLBACK_DATE = "2035-01-01";
+const FALLBACK_TRIAGER = "Fallback";
+const CYCLE_LENGTH_DAYS = 7;
+const DAY_TO_MS = 60 * 60 * 1000;
+const CYCLE_LENGTH_MS = CYCLE_LENGTH_DAYS * 24 * DAY_TO_MS;
+
+function readTriage() {
+  let data = fs.readFileSync(TRIAGE_JSON_FILE);
+  let triage = JSON.parse(data);
+  return {
+    triage,
+    triagers: triage.triagers,
+    duties: triage["duty-end-dates"]
+  };
 }
 
-function currentTriager(json) {
-  let duties = json["duty-end-dates"];
-  let now = new Date();
+function writeTriage(json) {
+  let data = JSON.stringify(json, undefined, "  ");
+  fs.writeFileSync(TRIAGE_JSON_FILE, data);
+}
 
-  let date = Object.keys(duties).filter(date => new Date(date) >= now).sort()[0];
-  if (!date) {
-    return { error: "No one on duty!" };
+function nextTriager(triagers, current_triager) {
+  let names = Object.keys(triagers);
+  let index = names.findIndex(t => t === current_triager);
+  if (index < 0) {
+    throw `\n**** FATAL: duty-end-dates refers an unexisting triager '${current_triager}'\n`;
+  }
+  
+  // exclude the fallback entry
+  let max = names.length - 1;
+  ++index;
+  if (index >= max) {
+    index = 0;
   }
 
-  const name = duties[date];
-  return Object.assign({
-    name, till: date, toString: function () {
-      return `${this.name}, on duty until ${this.till}`
-    }
-   }, json.triagers[name]);
+  return names[index];
 }
+
+function commandUpdate() {
+  let { triage, triagers, duties } = readTriage();
+
+  let last_date = Object.keys(duties)
+    .filter(date => date !== FALLBACK_DATE)
+    .slice(-1);
+  if (!last_date) {
+    throw "\n**** UNEXPECTED: The duty calendar is empty\n";
+  }
+
+  // remove...
+  delete duties[FALLBACK_DATE];
+
+  [last_date] = last_date;
+  let last_triager = duties[last_date];
+  let next_triager = last_triager;
+  let next_date_ms = new Date(last_date).getTime();
+
+  do {
+    next_date_ms += CYCLE_LENGTH_MS;
+    next_triager = nextTriager(triagers, next_triager);
+
+    let date = new Date(next_date_ms).toISOString().replace(/T.*$/, "");
+    duties[date] = next_triager;
+    console.log(`Added: until ${date} duty ${next_triager}`);
+  } while (next_triager !== last_triager);
+
+  // ...and readd to keep the list kinda sorted.
+  duties[FALLBACK_DATE] = FALLBACK_TRIAGER;
+
+  writeTriage(triage);
+  console.log("\nDon't forget to run 'npm run push' to publish the changes\n");
+}
+
+function commandPrepush() {
+  const { triage, triagers, duties } = readTriage();
+  
+  const ical = require('ical-toolkit');
+  let builder = ical.createIcsFileBuilder();
+
+  builder.calname = "Necko Triage";
+  builder.timezone = "UTC";
+  builder.tzid = "UTC";
+  builder.additionalTags = {
+    'REFRESH-INTERVAL': 'VALUE=DURATION:P1H'
+  };
+
+  for (let duty_date in duties) {
+    let duty_triager = duties[duty_date];
+    // Make the end-date inclusive
+    duty_date = new Date(new Date(duty_date).getTime() + DAY_TO_MS);
+    builder.events.push({
+      start: new Date(duty_date - CYCLE_LENGTH_MS),
+      end: new Date(duty_date),
+      summary: `Necko triager: ${duty_triager}`,
+      allDay: true,
+    });
+  }
+
+  let data = builder.toString();
+  fs.writeFileSync(TRIAGE_ICAL_FILE, data);
+  console.log(`Updated ${TRIAGE_ICAL_FILE}`);
+}
+
+function commandExempt(exempt_date) {
+  let { triage, triagers, duties } = readTriage();
+  if (!(exempt_date in duties)) {
+    throw `\n**** ERROR: The end date '${exempt_date}' not found in the duty list\n`;
+  }
+
+  console.log(`Removing '${duties[exempt_date]}' on duty till ${exempt_date} from the duty list`);
+  
+  exempt_date = new Date(exempt_date);
+  let dates_to_shift = Object.keys(duties).filter(date => {
+    return new Date(date).getTime() >= exempt_date;
+  });
+
+  for (
+    let date = dates_to_shift.shift();
+    date !== FALLBACK_DATE;
+    date = dates_to_shift.shift()
+  ) {
+    let next_date = dates_to_shift[0];
+    if (next_date === FALLBACK_DATE) {
+      delete duties[date];
+    } else {
+      duties[date] = duties[next_date];
+    }
+  }
+
+  writeTriage(triage);
+}
+
+let args = process.argv.slice(2);
+let command = args.shift();
+
+switch (command) {
+  case "update":
+    commandUpdate();
+    break;
+  case "exempt":
+    commandExempt(args[0]);
+    break;
+  case "prepush":
+    commandPrepush();
+    break;
+  default:
+    console.log(help_string);
+}
+
+console.log("Done\n");
